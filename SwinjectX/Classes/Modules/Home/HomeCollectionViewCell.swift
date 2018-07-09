ddf710b4d27058c02cf0ce43d367e2deedb36575
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-07-09 18:53:34 +0700

    Add sample for collection view

---
+++ SwinjectX/Classes/Modules/Home/HomeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var uiLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        // Initialization code
+    }
+}
--- SwinjectX/Classes/Modules/Home/HomeViewController.swift
+++ SwinjectX/Classes/Modules/Home/HomeViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -10,10 +12,41 @@ class HomeViewController: UIViewController {
         self.navigationItem.backBarButtonItem = UIBarButtonItem(
             title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil
         )
+
+        let homeCollectionViewCell = UINib(nibName: "HomeCollectionViewCell", bundle: nil)
+        self.homeCollectionView.register(homeCollectionViewCell, forCellWithReuseIdentifier: "HomeCollectionViewCell")
+        self.homeCollectionView.delegate = self
+        self.homeCollectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+extension HomeViewController:
+         UICollectionViewDelegate,
+         UICollectionViewDataSource,
+         UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell",
+                                                    for: indexPath) as! HomeCollectionViewCell
+        cell.uiLabel.text = "Halo from cell"
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width, height: 50)
     }
 }
